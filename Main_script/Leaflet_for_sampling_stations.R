260d8fbb717673d950f6ca3ea1b7c187d34f8af0
cmarchese <167329731+argonauta74@users.noreply.github.com>
2024-05-24 10:09:29 +0200

    Update Leaflet_for_sampling_stations.R

--- Main_script/Leaflet_for_sampling_stations.R
+++ Main_script/Leaflet_for_sampling_stations.R
@@ -1,6 +1,6 @@
 # ---------------------------------------------------------------------------------------------------------------------------
 # Script Description: 
-# This script generates an interactive leaflet map to facilitate the visualization of sampling stations for specific coastal ocean areas. 
+# This script generates an interactive leaflet map to facilitate the visualization of sampling stations for specific ocean areas. 
 # Colored markers denote each station and include pop-up windows displaying basic station-specific (e.g., lat, lon, date, etc.) information. 
 # The leaflet integrates multiple layers (i.e., environmental parameters), such as bathymetry and chlorophyll satellite data; 
 # however, other layers can be added according to the user's needs. 
